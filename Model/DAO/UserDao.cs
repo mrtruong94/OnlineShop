f86fb1101fc737233c3c095a43674b2881bbd247
Dinh Truong <truongdinh94@gmail.com>
2017-07-31 16:54:48 -0700

    Add the Funtion: Delete User

--- Model/DAO/UserDao.cs
+++ Model/DAO/UserDao.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model.EF;
+using PagedList.Mvc;
+using PagedList;
 
 namespace Model.DAO
 {   
@@ -22,10 +24,41 @@ namespace Model.DAO
             db.SaveChanges();
             return entity.ID;
         }
+        //Lay du lieu du db
+        public IEnumerable<User> ListAll(int pageNumber, int pageSize) // Get all list User
+        {
+            return db.Users.OrderByDescending(x => x.CreateDate).ToPagedList(pageNumber, pageSize);
+        }
+        //Update du lieu
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+        public bool Update(User enity)
+        {
+            try
+            {
+                var user = db.Users.Find(enity.ID);
+                user.Name = enity.Name;
+                user.Email = enity.Email;
+                user.ModifieldBy = enity.ModifieldBy;
+                user.ModifieldDate = enity.ModifieldDate;
+                user.CreateDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+       
         public User GetByID(string userName)
         {
             return db.Users.SingleOrDefault(x=>x.UserName == userName);
         }
+
         public int Login(string userName, string passWord)
         {
             var result = db.Users.SingleOrDefault(x => x.UserName == userName);
@@ -47,5 +80,23 @@ namespace Model.DAO
                 }
             }
         }
+        //Xoa du lieu tren db
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+           
+
+        }
+
+        
     }
 }
--- OnlineShop/Areas/Admin/Controllers/UserController.cs
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -12,25 +12,36 @@ namespace OnlineShop.Areas.Admin.Controllers
     public class UserController : BaseController
     {
         // GET: Admin/User
-        public ActionResult Index()
+        public ActionResult Index(int pageNumber = 1, int pageSize = 10)
         {
-            return View();
+            var dao = new UserDao();
+            var model = dao.ListAll(pageNumber, pageSize);
+            return View(model);
         }
+
         [HttpGet] //Dành cho return 1 View
         public ActionResult Create()
         {
             return View();
-        }    
+        }  
+ 
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDao().ViewDetail(id);
+            return View(user);
+        }  
         [HttpPost] //Dành cho post thông tin tạo mới user
+
+        //phương thức tạo mới user
         public ActionResult Create(User user)
         {
             if (ModelState.IsValid) //Nếu có validate form
             {
                 var dao = new UserDao();
-
                 var encrytedPass = Encrypt.MD5Hash(user.Password);
                 user.Password = encrytedPass;
                 long id = dao.Insert(user);
+               
                 if (id > 0)
                 {
                     return RedirectToAction("Index", "User");
@@ -41,6 +52,39 @@ namespace OnlineShop.Areas.Admin.Controllers
                 }
             }
             return View("Index");
+        }
+        [HttpPost]
+        // phương thức edit user
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid) //Nếu có validate form
+            {
+                var dao = new UserDao();
+                //kiểm tra mật khẩu nhập vào khác rỗng mới thực hiện update
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encrytedPass = Encrypt.MD5Hash(user.Password);
+                    user.Password = encrytedPass;
+                }
+                var result = dao.Update(user);
+
+                if (result)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật User không thành công!");
+                }
+            }
+            return View("Index");
+        }
+        [HttpDelete]
+        //Xoa user
+        public ActionResult Delete(int id)
+        {
+            new UserDao().Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
